035027d8997901b79ba07e3009d35b4e144cb274
DavidSotoR <soto_rd@hotmail.com>
2024-05-02 09:15:27 -0600

    Shimpemnt data
    se cambio lista de details por una tabla

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -20,7 +20,7 @@ function Login() {
       <div className="mt-5 d-flex flex-column justify-content-center align-items-center">
           <div className="d-grid">
             <div className="d-flex" style={{ width: "100%" }}>
-              <h3 className="text-start">Iniciar Sesión</h3>
+              <h3 className="text-start">Servicio Becas</h3>
             </div>
             <div className="mb-3">
               <label htmlFor="exampleFormControlInput1" className="form-label text-align-right">Usuario</label>
@@ -29,6 +29,15 @@ function Login() {
             <div className="mb-3">
               <label htmlFor="exampleFormControlInput2" className="form-label text-align-left">Contraseña</label>
               <input type="password" className="form-control" id="exampleFormControlInput2" placeholder="Contraseña"/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="login-type"> Tipo de usuario </label>
+              <select id="login-type" class="form-select">
+                <option value="1">Adminitrados</option>
+                <option value="2">Colaborador</option>
+                <option value="3">Calidad</option>
+                <option value="4">Postulante</option>
+              </select>
             </div>
             <div className="mb-3">
               <button onClick={ sendLogin } className="btn btn-primary">Iniciar Sesión</button>
