4a4d5400a682e2b20050eb550be967babd24d1e9
DavidSotoR <soto_rd@hotmail.com>
2024-05-28 21:09:22 -0600

    familia new
    inicio formulario nueva familia

--- src/components/Catalogos/Familias/FamiliaAlta.js
+++ src/components/Catalogos/Familias/FamiliaAlta.js
@@ -1,7 +1,19 @@
 function FamiliaAlta() {
     return (
-        <div className="container">
-            Awui va el alta de familia
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col">
+                    <h3>SOLICITUD PARA EL ESTUDIO SOCIOECONÓMICO 1/2</h3>
+                </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col-2 pt-1">
+                    FAMILIA:
+                </div>
+                <div className="col">
+                    <input type="text" id="inputFamiliaName" class="form-control" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Catalogos/Familias/Familias.js
+++ src/components/Catalogos/Familias/Familias.js
@@ -1,12 +1,61 @@
 import { Link } from "react-router-dom";
 import PathConstants from "../../../routes/pathsConstants";
+import { useState } from "react";
 
 function CatalogoFamilias() {
+    const [ allFamilias, setAllFamilias ] = useState([
+        { id: 1, nombre: 'Soto Resendiz', colegio: "Alfa & Omega" }
+    ])
+
+    const renderFilasTablaFamilias = () => {
+        return allFamilias.map((familia, index) => (
+            <tr key={'tr-'+index}>
+                <td>
+                    <p style={{ fontWeight: "bold" }}>{familia.id}</p>
+                </td>
+                <td>
+                    <p style={{ fontWeight: "bold" }}>{familia.nombre}</p>
+                </td>
+                <td>
+                    <p style={{ fontWeight: "bold" }}>{familia.colegio}</p>
+                </td>
+                <td>
+                    <div className="d-flex">
+                        <button className="btn btn-primary btn-sm mx-1">Editar</button>
+                        <button className="btn btn-danger btn-sm mx-1">Borrar</button>
+                    </div>
+                </td>
+            </tr>
+        ));
+    };
     
     return (
-       <div>
-            Catalogo Familias
-            <Link className="btn btn-primary" to={PathConstants.FAMILIASALTA}>Agregar Familia</Link>
+       <div className="container mt-3">
+            <div className="row">
+                <div className="col">
+                    <h3>Catalogo Familias</h3>
+                </div>
+                <div className="col">
+                    <Link className="btn btn-primary" to={PathConstants.FAMILIASALTA}>Agregar Familia</Link>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">FAMILIA</th>
+                                <th scope="col">COLEGIO</th>
+                                <th scope="col">Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { renderFilasTablaFamilias() }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
        </div> 
     )
 }
