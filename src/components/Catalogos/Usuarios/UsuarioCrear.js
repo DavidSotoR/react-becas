9edc97518d0f6e388fc04e7468ab0539eb82eb4f
DavidSotoR <soto_rd@hotmail.com>
2024-06-01 09:08:53 -0600

    formulario usuario
    se hizo el formulario usuario , falta toamr el perfil

---
+++ src/components/Catalogos/Usuarios/UsuarioCrear.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function UsuarioCrear({ onCreate }) {
+    const [listaPerfiles, setListaPerfiles] = useState([])
+    const [ dataPostUsuario, setDataPostUsuario ] = useState({
+        name:"",
+        email:"",
+        password:"",
+        password_confirmation:"",
+        id_perfil: 1
+    })
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    }
+    const getPerfilesList = async () => {
+        try {
+            const resp = await axios.get('http://localhost:8000/api/auth/perfiles', config);
+            setListaPerfiles(resp.data);
+        } catch (error) {
+            setListaPerfiles([])
+            console.error("Error fetching perfiles:", error);
+        }
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        console.log(e);
+        setDataPostUsuario(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+
+        onCreate(dataPostUsuario)
+
+    };
+
+    const agregarOpcionesSelect = () => {
+        return listaPerfiles.map((perfil,index)=>(
+            <option key={perfil.id} value={perfil.id} selected={index === 0}>
+                {perfil.nombre}
+            </option>
+            
+        ))
+    }
+
+    useEffect(()=>{
+        getPerfilesList()
+        console.log('Effect Usuarios Crear');
+    }, [])
+
+    return (
+            <div className="container">
+            <div class="mb-3">
+                <label htmlFor="inputName" class="form-label">Nombre:</label>
+                <input type="text" class="form-control" id="inputName" name="name"
+                        placeholder="Nombre:"
+                        value={dataPostUsuario.name}
+                        onChange={handleInputChange}/>
+            </div>
+            <div class="mb-3">
+                <label htmlFor="inputEmail" class="form-label">Email</label>
+                <input type="email" class="form-control" id="inputEmail" name="email"
+                        placeholder="name@example.com"
+                        value={dataPostUsuario.email}
+                        onChange={handleInputChange}/>
+            </div>
+            <div className="mb-3">
+                    <label htmlFor="inputPassword" class="form-label">Password</label>
+                    <input type="password" id="inputPassword" class="form-control" 
+                    aria-describedby="passwordHelpBlock" name="password" value={dataPostUsuario.password}
+                    onChange={handleInputChange}/>
+                    <div id="passwordHelpBlock" class="form-text">
+                        El password debe contener al menos una letra mayuscula y numeros.
+                    </div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="inputPasswordConfirmar" class="form-label">Confirmar Password</label>
+                <input type="password" id="inputPasswordConfirmar" name="password_confirmation"
+                        className="form-control" 
+                        aria-describedby="passwordHelpBlock"
+                        value={dataPostUsuario.password_confirmation}
+                        onChange={handleInputChange}/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="inputPerfil" class="form-label">Perfil</label>
+                <select id="inputPerfil" 
+                        name="id_perfil"
+                        className="form-select" 
+                        aria-label="Default select example"
+                        value={dataPostUsuario.id_perfil}
+                        onChange={handleInputChange}>
+                    { agregarOpcionesSelect() }
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default UsuarioCrear;
--- src/components/Catalogos/Usuarios/Usuarios.js
+++ src/components/Catalogos/Usuarios/Usuarios.js
@@ -1,12 +1,95 @@
 import axios from "axios";
-import { useState } from "react";
+import React,{ useEffect, useState } from "react";
+import UsuarioCrear from "./UsuarioCrear";
 
 function Usuarios() {
     const [ allUsuarios, setAllUsuarios ] = useState([])
+    const [ dataPostUsuario, setDataPostUsuario ] = useState({})
 
-    const getAllDataUsuarios = async () => {
-        var resp = await axios.get('http://localhost:8000/api/auth/usuarios')
+    const config = {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
     }
+    const getAllDataUsuarios = async () => {
+        try {
+            const resp = await axios.get('http://localhost:8000/api/auth/usuarios', config);
+            console.log(resp);
+            setAllUsuarios(resp.data);
+        } catch (error) {
+            console.error("Error fetching perfiles:", error);
+        }
+    }
+
+    const getNamePerfil = (id) => {
+        var name = ''
+        switch (id) {
+            case 1:
+                name = 'Administrador'
+                break;
+            case 2:
+                name = 'Gerencia'
+                break;
+
+            case 3:
+                name = 'Calidad'
+                break;
+
+            case 4:
+                name = 'Colaboradores'
+                break;
+
+            case 5:
+                name = 'Empresas'
+                break;
+
+            case 6:
+                name = 'Familias'
+                break;
+            default:
+                name = 'Sin Identificar'
+                break;
+        }
+
+        return name
+    }
+
+    const getDatosPost = (datosPost) => {
+        setDataPostUsuario(datosPost)
+    }
+
+    const sendDataPost = () => {
+        console.log(dataPostUsuario);
+    }
+
+    useEffect( ()=>{
+        getAllDataUsuarios();
+    }, [])
+
+    const renderFilasTablaUsuarios = () => {
+        return allUsuarios.map((usuario, index) => (
+            <tr key={'tr-usuario-'+index}>
+                <td>
+                    <p>{usuario.id}</p>
+                </td>
+                <td>
+                    <p>{usuario.name}</p>
+                </td>
+                <td>
+                    <p>{usuario.email}</p>
+                </td>
+                <td>
+                    <p>{ getNamePerfil(usuario.id_perfil)}</p>
+                </td>
+                <td>
+                    <div className="d-flex justify-content-start">
+                        <button className="btn btn-primary mx-1">Editar</button>
+                        <button className="btn btn-small btn-danger mx-1">X</button>
+                    </div>
+                </td>
+            </tr>
+        ));
+    };
     
     return (
         <div className="container mt-3">
@@ -15,12 +98,12 @@ function Usuarios() {
                     <h4>Catalogo de Usuarios</h4>
                 </div>
                 <div className="col">
-                    <button className="btn btn-primary">Agregar Usuario</button>
+                    <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Agregar Usuario</button>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                <table class="table">
+                <table className="table">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
@@ -31,10 +114,28 @@ function Usuarios() {
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        { renderFilasTablaUsuarios() }
                     </tbody>
                 </table>
                 </div>
+            </div>
+
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Crear Nuevo Usuario</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <UsuarioCrear onCreate={ getDatosPost }/>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary" onClick={ sendDataPost }>Crear</button>
+                </div>
+                </div>
+            </div>
             </div>
 
         </div>
