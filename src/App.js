88a3f737dfb970d360e2e4609267caa252332ae5
DavidSotoR <soto_rd@hotmail.com>
2024-08-16 22:17:27 -0600

    listener para cambio en el store en desarrollo
    se esta apliacndo una funcion para detectar cuando se acabe la sesion en diferentes pestañas

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout/layout';
 import NotFound from './components/NotFound/NotFound';
 import routes from './routes/routes';
-import { Suspense } from 'react';
-import { AuthProvider } from './context/AuthContext';
+import { Suspense, useContext, useEffect } from 'react';
+import { AuthContext, AuthProvider } from './context/AuthContext';
 
 function App() {
+  const { logout } = useContext(AuthContext);
+  useEffect(() => {
+    const handleStorageChange = (e) => {
+      if (e.key === 'token') { // Reemplaza 'yourTokenKey' por la clave que uses para almacenar tu token
+        console.log(`Token cambiado: ${e.newValue}`);
+        //logout()
+        // Aquí puedes manejar el nuevo valor del token
+        // Por ejemplo, actualizar el estado o realizar una acción
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    // Limpia el evento al desmontar el componente
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
   const router = createBrowserRouter([
     {
       element: <Layout/>,
@@ -18,11 +37,9 @@ function App() {
   ])
 
   return (
-    <AuthProvider>
       <Suspense fallback={<div>Loading...</div>}>
         <RouterProvider router={router} />
       </Suspense>
-    </AuthProvider>
   )
 }
 
