c113e98bc8052d21c99afc575d59e41df8c029f9
Jesus Aguilar <jesus.aguilar@getic.mx>
2024-08-30 08:14:38 -0600

    Se añade informacionde Padre y madre al alta de la familia

--- src/components/Catalogos/ServicioEstudio/AltaEstudioSocioeconomico.js
+++ src/components/Catalogos/ServicioEstudio/AltaEstudioSocioeconomico.js
@@ -40,21 +40,86 @@ function AltaEstudioSocioeconomico(){
         email:'',
         telefono_movil:'',
         telefono_contacto:'',
+        padre:{
+            id_familias_padres_tipo:'1',
+            nombre:'',
+            edad:'',
+            vive:'',
+            direccion:'',
+            ocupacion_actual:'',
+            empresa_trabajo:'',
+            email:'',
+            telefono_casa:'',
+            contecto_principal:false,
+        },
+        madre:{
+            id_familias_padres_tipo:'1',
+            nombre:'',
+            edad:'',
+            vive:'',
+            direccion:'',
+            ocupacion_actual:'',
+            empresa_trabajo:'',
+            email:'',
+            telefono_casa:'',
+            contecto_principal:false,
+        }
     })
 
+    /*
+    const[fromDataPapa,setFromDataPapa] = useState({
+        id_familias_padres_tipo:'1',
+        nombre:'',
+        edad:'',
+        vive:'',
+        direccion:'',
+        ocupacion_actual:'',
+        empresa_trabajo:'',
+        email:'',
+        telefono_casa:'',
+        contecto_principal:false,
+    })
+
+    const[fromDataMama,setFromDataMama] = useState({
+        id_familias_padres_tipo:'2',
+        nombre:'',
+        edad:'',
+        vive:'',
+        direccion:'',
+        ocupacion_actual:'',
+        empresa_trabajo:'',
+        email:'',
+        telefono_casa:'',
+        contecto_principal:false,
+    })
+    */
+
+
     const formInputChange =(e) => {
-        var {name, value, type, checked } = e.target
-        if(type === 'checkbox'){
-            setFormData(prevState => ({
-                ...prevState,
-                [name]: checked
-            }));
-        }else{
-            setFormData(prevState => ({
-                ...prevState,
-                [name]: value
-            }));
+        var {name, value, type, checked } = e.target;
+        const updatedValue = type === 'checkbox' ? checked : value;
+        
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: updatedValue
+        }));
+    }
+    
+    const formInputChangeFamiliar = (e,familiar='') => {
+        if(familiar ===''){
+            return false;
         }
+
+        var {name, value, type, checked } = e.target;
+        const updatedValue = type === 'checkbox' ? checked : value;
+        
+        setFormData(prevState => ({
+            ...prevState,
+            [familiar]:{
+                ...prevState[familiar],
+                [name]: updatedValue
+            }
+        }));
     }
     
     const sendDataEstudioSocioeconomico = () =>{
@@ -158,7 +223,163 @@ function AltaEstudioSocioeconomico(){
     },[fromData.id_cliente,fromData.es_cliente_comun])
     
     
-    
+    const formularioFamilia = (familiar = '') => {
+        if(familiar === '') {
+            return ''
+        }
+        return (
+            <>
+            <h4>{familiar.charAt(0).toUpperCase() + familiar.slice(1)}</h4>
+            <hr/>
+            
+            <div className="mb-3 row">
+                <label htmlFor="nombre" className="col-sm-2 col-form-label">
+                    Nombre
+                </label>
+                <div className="col-sm-10">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="nombre" 
+                        name="nombre"
+                        value={fromData[familiar].nombre}
+                        onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                    />
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <label htmlFor="edad" className="col-sm-2 col-form-label">
+                    Edad
+                </label>
+                <div className="col-sm-10">
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="edad" 
+                        name="edad"
+                        value={fromData[familiar].edad}
+                        onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                    />
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <p className="col-sm-2 col-form-label">
+                    Vivie
+                </p>
+                <div className="col-sm-10 pt-1">
+                    <div className="form-switch">
+                        <input 
+                            className="form-check-input" 
+                            id="vive" 
+                            name="vive" 
+                            type="checkbox" 
+                            checked={fromData[familiar].vive}
+                            role="switch" 
+                            onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                            />
+                        <label className="form-check-label">{(fromData[familiar].vive) ? 'Si' : 'No'}</label>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <label htmlFor="direccion" className="col-sm-2 col-form-label">
+                    Direccion:
+                </label>
+                <div className="col-sm-10">
+                    <textarea
+                        id="direccion"
+                        name="direccion"
+                        value={fromData[familiar].direccion}
+                        onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                        placeholder="Dirección..."
+                        rows="4"
+                        cols="10"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <label htmlFor="ocupacion_actual" className="col-sm-2 col-form-label">
+                    Ocupacion actual:
+                </label>
+                <div className="col-sm-10">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="ocupacion_actual" 
+                        name="ocupacion_actual"
+                        value={fromData[familiar].ocupacion_actual}
+                        onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                    />
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <label htmlFor="empresa_trabajo" className="col-sm-2 col-form-label">
+                    Empresa de trabajo:
+                </label>
+                <div className="col-sm-10">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="empresa_trabajo" 
+                        name="empresa_trabajo"
+                        value={fromData[familiar].empresa_trabajo}
+                        onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                    />
+                </div>
+            </div>
+
+            <div className="mb-3 row">
+                <label htmlFor="email" className="col-sm-2 col-form-label">Correo:</label>
+                <div className="col-sm-10">
+                    <input type="text" className="form-control" id="email" name="email" onChange={(e)=> formInputChangeFamiliar(e,familiar)}/>
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <label htmlFor="telefono_casa" className="col-sm-2 col-form-label">
+                    Telefono de casa:
+                </label>
+                <div className="col-sm-10">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="telefono_casa" 
+                        name="telefono_casa"
+                        value={fromData[familiar].telefono_casa}
+                        onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                    />
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <p className="col-sm-2 col-form-label">
+                    Contacto Principal:
+                </p>
+                <div className="col-sm-10 pt-1">
+                    <div className="form-switch">
+                        <input 
+                            className="form-check-input" 
+                            id="contecto_principal" 
+                            name="contecto_principal" 
+                            type="checkbox" 
+                            checked={fromData[familiar].contecto_principal}
+                            role="switch" 
+                            onChange={(e)=> formInputChangeFamiliar(e,familiar)}
+                            />
+                        <label className="form-check-label">{(fromData[familiar].contecto_principal) ? 'Si' : 'No'}</label>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            </>
+        )
+    }
 
 
     return(<>
@@ -199,16 +420,16 @@ function AltaEstudioSocioeconomico(){
             <div className="row">
                 <div className="col-md-2"/>
                 <div className="col-md-8">
-                    {
-                    //JSON.stringify(ordenesServicio)
-                    }
 
+                    <h4>Familia:</h4>
+                    <hr/>
                     <div className="mb-3 row">
-                        <label htmlFor="candidato" className="col-sm-2 col-form-label">Familia</label>
+                        <label htmlFor="candidato" className="col-sm-2 col-form-label">Nombre de Familia:</label>
                         <div className="col-sm-10">
                             <input type="text" className="form-control" id="candidato" name="candidato" onChange={(e)=> formInputChange(e)}/>
                         </div>
                     </div>
+
                     <div className="mb-3 row">
                         <label htmlFor="situacion" className="col-sm-2 col-form-label">Situacion:</label>
                         <div className="col-sm-10">
@@ -224,6 +445,7 @@ function AltaEstudioSocioeconomico(){
                             />
                         </div>
                     </div>
+
                     <div className="mb-3 row">
                         <label htmlFor="email" className="col-sm-2 col-form-label">Correo:</label>
                         <div className="col-sm-10">
@@ -247,10 +469,9 @@ function AltaEstudioSocioeconomico(){
                             </div>
                         </div>
                     </div>
-
                     {fromData.es_cliente_comun && (
                         <div className="mb-3 row">
-                            <label htmlFor="colegios_comunes" className="col-sm-2 col-form-label">Colegios Comunes</label>
+                            <label htmlFor="colegios_comunes" className="col-sm-2 col-form-label">Familia colegios relacionados:</label>
                             <div className="col-sm-10">
                                 <Select options={ clientesComunes } onChange={(e)=>handlerChangeSelectClientes(e)}
                                 components={animatedComponents}
@@ -260,9 +481,19 @@ function AltaEstudioSocioeconomico(){
                             </div>
                         </div>
                     )}
+
+                    <br/>
+
+                    {formularioFamilia('padre')}
+
+                    <br/>
+
+                    {formularioFamilia('madre')}
+
                     <div className="d-flex" style={{ flexDirection: 'row-reverse'}}>
                         <button className="btn btn-primary btn-sm fw-bold " onClick={sendDataEstudioSocioeconomico}>Guardar</button>
                     </div>
+                    
                     <br/>
                     {JSON.stringify(fromData)}
                     
