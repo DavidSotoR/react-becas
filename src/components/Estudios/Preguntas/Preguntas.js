fffa092b1ea06e9421198e3caa1c9147ce3f69c4
Jesus Aguilar <jesus.aguilar@getic.mx>
2024-10-09 21:42:06 -0600

    leer y enviar respuestas

--- src/components/Estudios/Preguntas/Preguntas.js
+++ src/components/Estudios/Preguntas/Preguntas.js
@@ -1,6 +1,7 @@
 import Respuestas from "./Respuestas"
 
-export default function Preguntas({preguntas}) {
+export default function Preguntas({idEstudio,preguntas}) {
+
     return preguntas && preguntas.map((pregunta,index) => (<>
         <div  key={'ps-'+index} className="mt-1" >
             <div className="row">
@@ -8,12 +9,14 @@ export default function Preguntas({preguntas}) {
                     <h5>{pregunta?.numero_pregunta && pregunta.numero_pregunta+' - '} {pregunta.pregunta}</h5>
                 </div>
             </div>
-            {pregunta.longitud_respuesta}
+            
             <Respuestas 
+                idEstudio={idEstudio}
                 idPregunta={pregunta.id}
                 idPreguntaTipo={pregunta.id_catalogo_encuestas_preguntas_tipo} 
                 longitudRespuesta={pregunta.longitud_respuesta} 
                 />
+
         </div>
-        </>))
+    </>))
 }
