17fead3c1796668c3bc43bae9098ed3736aea1c7
Jesus Aguilar <jesus.aguilar@getic.mx>
2025-01-23 22:21:30 -0600

    se re acomoda tamaño de texto de tabla de encuetas realizadas

--- src/components/HomePageEmpresa/ListaEncuestas/TablaEncuestas.js
+++ src/components/HomePageEmpresa/ListaEncuestas/TablaEncuestas.js
@@ -90,7 +90,7 @@ export default function TablaEncuestas({
             </div>
           ),
           sortable: true,
-          width: "300px",
+          width: "150px",
         },
 
         mostrarColumnaHijo() && {
@@ -99,7 +99,7 @@ export default function TablaEncuestas({
                 <span>{row.nombre_hijo}</span> 
           ),
           sortable: true,
-          width: "300px",
+          width: "250px",
         },
         ,
         ...listaParametros.map((parametro) => ({
