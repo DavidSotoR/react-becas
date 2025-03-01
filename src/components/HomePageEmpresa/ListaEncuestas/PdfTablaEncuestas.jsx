0ce72b7b9becad0114d2a597a395eaf5019870d8
MachuchoESG <sistemas@esg.com.mx>
2025-03-01 12:57:31 -0600

    update - agrega o quita columna hijo
    en reportes de % de beca asignado se actualizo para agregar columna hijo dependiendo si es BP7 agrega y si no lo remueve

--- src/components/HomePageEmpresa/ListaEncuestas/PdfTablaEncuestas.jsx
+++ src/components/HomePageEmpresa/ListaEncuestas/PdfTablaEncuestas.jsx
@@ -8,20 +8,11 @@ import {
 } from "lib/estudios-functions";
 import { jsPDF } from "jspdf";
 import "jspdf-autotable";
-import axios from "../../../../node_modules/axios/index";
 export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
-
-  const APIURL = process.env.REACT_APP_API_URL;
-  const config = {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  };
-  
   const mostrarColumnaHijo = () => {
-      return data.some(row => row.hasOwnProperty('hijo'));
-    };
-  
+    return data.some((row) => row.hasOwnProperty("hijo"));
+  };
+
   const columns = [
     {
       name: "No Estudio",
@@ -32,8 +23,8 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
       selector: (row) => row.candidato,
       width: "300px",
     },
-    
-    mostrarColumnaHijo() && {      
+
+    mostrarColumnaHijo() && {
       name: "Hijo",
       selector: (row) => row.nombre_hijo,
       width: "300px",
@@ -56,46 +47,77 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
     },
     {
       name: "Porcentaje Otorgado",
-      cell: (row) =>  {return (row.porcentaje_otorgado !== null )  ?  `${row.porcentaje_otorgado}%` : row.hijo ? row.hijo.porcentaje_otorgado : 'SIN DATO' },
+      cell: (row) => {
+        return row.hijo === undefined || row.hijo === null
+          ? `${row.porcentaje_otorgado} %`
+          : row.hijo
+          ? `${row.hijo.porcentaje_otorgado} %`
+          : "SIN DATO";
+      },
     },
     {
       name: "No. Familia Colegio",
-      cell: (row) => {return (row.clave_familia_colegio !==null )  ? row.clave_familia_colegio : "" },
+      cell: (row) => {
+        return row.clave_familia_colegio !== null
+          ? row.clave_familia_colegio
+          : "";
+      },
     },
 
-    
     {
       name: "Proyecto",
-      cell: (row) => {return (row?.proyecto && row.proyecto?.nombre && row.proyecto.nombre !==null )  ? row.proyecto.nombre : "" },
+      cell: (row) => {
+        return row?.proyecto &&
+          row.proyecto?.nombre &&
+          row.proyecto.nombre !== null
+          ? row.proyecto.nombre
+          : "";
+      },
       width: "300px",
     },
-    
+
     {
       name: "Orden de servicio",
-      cell: (row) => {return (row?.orden_servicio && row.orden_servicio?.descripcion &&row.orden_servicio.descripcion !==null )  ?  `#${row.orden_servicio.id} ${row.orden_servicio.descripcion}` : "" },
+      cell: (row) => {
+        return row?.orden_servicio &&
+          row.orden_servicio?.descripcion &&
+          row.orden_servicio.descripcion !== null
+          ? `#${row.orden_servicio.id} ${row.orden_servicio.descripcion}`
+          : "";
+      },
       width: "300px",
     },
   ];
-  
+
   const transformDataForExcel = () => {
     return data.map((row) => {
       console.log(row);
-      
+
       const transformedRow = {};
-      columns.forEach((col) => {        
+      columns.forEach((col) => {
         if (col.selector) {
           // Si el selector es una función, evaluarlo
-          transformedRow[col.name] = typeof col.selector === "function" ? col.selector(row) : row[col.selector];
+          transformedRow[col.name] =
+            typeof col.selector === "function"
+              ? col.selector(row)
+              : row[col.selector];
         } else if (col.cell) {
           // Para celdas con lógica compleja, agregar texto representativo
-          transformedRow[col.name] = typeof col.cell === "function"
-                    ? col.cell(row)
-                    : "null";
+          transformedRow[col.name] =
+            typeof col.cell === "function" ? col.cell(row) : "null";
         }
       });
 
       return transformedRow;
     });
+  };
+
+  const contieneHijos = () => {
+    console.log('ejecuta validacion hijo');
+    console.log(data);
+    return data.some(
+      (element) => element.hijo !== null && element.hijo !== undefined
+    );
   };
 
   const getHeadersParameters = async () => {
@@ -104,7 +126,7 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
       "NUMERO DE HIJOS INSCRITOS EN ESTE COLEGIO": "NUM. HIJOS",
       "ANTIGÜEDAD DE LA FAMILIA EN EL COLEGIO": "ANTIGÜEDAD EN COLEGIO",
     };
-  
+
     let headers = [];
     parametros.forEach((element) => {
       if (renamingMap[element.nombre]) {
@@ -113,57 +135,74 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
         headers.push(element.nombre);
       }
     });
-  
-    let headersTable = [
-      "FOLIO",
-      "FAMILIA",
-      "ALUMNO",
-      "TOTAL",
-      "% RECOMENDADO",
-      "% ASIGNADO"
-    ];
-    
-    headersTable.splice(3, 0, ...headers); // Insertar después de "TOTAL"
-    
+    let headersTable;
+    if (contieneHijos()) {
+      headersTable = [
+        "FOLIO",
+        "FAMILIA",
+        "ALUMNO",
+        "TOTAL",
+        "% RECOMENDADO",
+        "% ASIGNADO",
+      ];
+      headersTable.splice(3, 0, ...headers);
+    } else {
+      headersTable = [
+        "FOLIO",
+        "FAMILIA",
+        "TOTAL",
+        "% RECOMENDADO",
+        "% ASIGNADO",
+      ];
+      headersTable.splice(2, 0, ...headers);
+    }
+
+    // Insertar después de "TOTAL"
+    if (tipo_reporte === "porcentaje_asignado") {
+      if (contieneHijos()) {
+        return ["FOLIO", "FAMILIA", "ALUMNO", "% ASIGNADO"];
+      } else {
+        return ["FOLIO", "FAMILIA", "% ASIGNADO"];
+      }
+    }
     return headersTable;
   };
-  
 
   const exportToPdf = async () => {
     const doc = new jsPDF();
     const headers = await getHeadersParameters();
     const transformedData = transformDataForExcel();
-  
-    const proyectosUnicos = [...new Set(transformedData.map(item => item.Proyecto))];
+    console.log(transformedData);
+
+    const proyectosUnicos = [
+      ...new Set(transformedData.map((item) => item.Proyecto)),
+    ];
     let proyectos = proyectosUnicos;
-    
+
     const pageWidth = doc.internal.pageSize.getWidth();
     doc.setFontSize(8);
-    //console.log(headers);
-  
-    
 
     // Mapeo de los headers a los nombres correctos en el objeto de datos
     const headerToKeyMap = {
-        "FOLIO": "No Estudio",
-        "FAMILIA": "Familia",
-        "ALUMNO": "Hijo",
-        "TOTAL": "Total",
-        "% RECOMENDADO": "Porcentaje Sugerido",
-        "% ASIGNADO": "Porcentaje Otorgado",
-        "PATRIMONIO": "PATRIMONIO REPORTADO",
-        "NIVEL DE LIQUIDES": "NIVEL DE LIQUIDES",
-        "CALIDAD DE VIDA": "CALIDAD DE VIDA",
-        "NUM. HIJOS": "NUMERO DE HIJOS INSCRITOS EN ESTE COLEGIO",
-        "ANTIGÜEDAD EN COLEGIO": "ANTIGÜEDAD DE LA FAMILIA EN EL COLEGIO",
-        "PROMEDIO ACADÉMICO": "PROMEDIO ACADÉMICO",
-        "CONDUCTA": "CONDUCTA"
+      FOLIO: "No Estudio",
+      FAMILIA: "Familia",
+      ALUMNO: "Hijo",
+      TOTAL: "Total",
+      "% RECOMENDADO": "Porcentaje Sugerido",
+      "% ASIGNADO": "Porcentaje Otorgado",
+      PATRIMONIO: "PATRIMONIO REPORTADO",
+      "NIVEL DE LIQUIDES": "NIVEL DE LIQUIDES",
+      "CALIDAD DE VIDA": "CALIDAD DE VIDA",
+      "NUM. HIJOS": "NUMERO DE HIJOS INSCRITOS EN ESTE COLEGIO",
+      "ANTIGÜEDAD EN COLEGIO": "ANTIGÜEDAD DE LA FAMILIA EN EL COLEGIO",
+      "PROMEDIO ACADÉMICO": "PROMEDIO ACADÉMICO",
+      CONDUCTA: "CONDUCTA",
     };
-    
+
     console.log(transformedData);
-    
-    const body = transformedData.map(item =>
-        headers.map(header => item[headerToKeyMap[header]] || "") // Si no encuentra el valor, pone ""
+
+    const body = transformedData.map(
+      (item) => headers.map((header) => item[headerToKeyMap[header]] || "") // Si no encuentra el valor, pone ""
     );
 
     // Título del proyecto
@@ -172,33 +211,19 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
     let xPosition = (pageWidth - textWidthProyecto) / 2;
     doc.text(titleProyecto, xPosition, 10);
 
-    let ordenServicio = transformedData[0]["Orden de servicio"]
-    
+    let ordenServicio = transformedData[0]["Orden de servicio"];
+
     let OS = "Orden Servicio: " + ordenServicio;
     let textWidthOS = doc.getTextWidth(OS);
     let xPositionOS = (pageWidth - textWidthOS) / 2;
     doc.text(OS, xPositionOS, 15);
 
     console.log(body);
-    
-    
 
-    if (tipo_reporte === 'completo') {
-      doc.autoTable({
-        head: [headers], // Encabezados
-        body: body,      // Datos
-        startY: 20,      // Posición Y donde comienza la tabla
-        theme: "grid",   // Estilo de la tabla
-        styles: {
-          fontSize: 4, // Tamaño de la fuente
-          cellPadding: 1, // Espaciado interno de las celdas
-        },
-        headStyles: {
-          fillColor: [71, 209, 214], // Color de fondo del encabezado
-          textColor: [255, 255, 255], // Color del texto del encabezado
-          fontStyle: "bold", // Negritas en el encabezado
-        },
-        columnStyles: {
+    if (tipo_reporte === "completo") {
+      let columnas = {};
+      if (contieneHijos()) {
+        columnas = {
           0: { cellWidth: 10 }, // Ancho de la columna ID
           1: { cellWidth: "wrap" }, // Ancho de la columna Nombre
           2: { cellWidth: "auto" }, // Ancho de la columna Email
@@ -207,23 +232,23 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
           5: { cellWidth: "auto" }, // Ancho de la columna Orden de Servicio
           6: { cellWidth: "auto" }, // Ancho de la columna Proyecto
           7: { cellWidth: "auto" }, // Ancho de la columna Orden de Servicio
-        },
-      });
-    }
-
-    if (tipo_reporte === 'porcentaje_asignado') {
-      let headerPorcentaje = [
-        "FOLIO",
-        "FAMILIA",
-        "ALUMNO",
-        "% ASIGNADO"
-      ]
-
+        };
+      } else {
+        columnas = {
+          0: { cellWidth: 10 }, // Ancho de la columna ID
+          1: { cellWidth: "wrap" }, // Ancho de la columna Nombre
+          2: { cellWidth: "auto" }, // Ancho de la columna Email
+          3: { cellWidth: "auto" }, // Ancho de la columna Fecha
+          4: { cellWidth: "auto" }, // Ancho de la columna Proyecto
+          5: { cellWidth: "auto" }, // Ancho de la columna Orden de Servicio
+          6: { cellWidth: "auto" }, // Ancho de la columna Proyecto
+        };
+      }
       doc.autoTable({
-        head: [headerPorcentaje], // Encabezados
-        body: body,      // Datos
-        startY: 20,      // Posición Y donde comienza la tabla
-        theme: "grid",   // Estilo de la tabla
+        head: [headers], // Encabezados
+        body: body, // Datos
+        startY: 20, // Posición Y donde comienza la tabla
+        theme: "grid", // Estilo de la tabla
         styles: {
           fontSize: 4, // Tamaño de la fuente
           cellPadding: 1, // Espaciado interno de las celdas
@@ -233,16 +258,53 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
           textColor: [255, 255, 255], // Color del texto del encabezado
           fontStyle: "bold", // Negritas en el encabezado
         },
-        columnStyles: {
+        columnStyles: columnas,
+      });
+    }
+
+    if (tipo_reporte === "porcentaje_asignado") {
+      console.log("PORCRNTAJE REPORTE SIGNADO");
+
+      let headerPorcentaje;
+      let columnsPorcentaje;
+
+      if (contieneHijos()) {
+        console.log("PORCRNTAJE REPORTE SIGNADO - HIJOS");
+        headerPorcentaje = ["FOLIO", "FAMILIA", "ALUMNO", "% ASIGNADO"];
+        columnsPorcentaje = {
           0: { cellWidth: 10 },
           1: { cellWidth: "wrap" },
-          2: { cellWidth: "auto" }, 
-          3: { cellWidth: "auto" }, 
+          2: { cellWidth: "auto" },
+          3: { cellWidth: "auto" },
+        };
+      } else {
+        console.log("PORCRNTAJE REPORTE SIGNADO - SIN HIJOS");
+        headerPorcentaje = ["FOLIO", "FAMILIA", "% ASIGNADO"];
+        columnsPorcentaje = {
+          0: { cellWidth: 10 },
+          1: { cellWidth: "wrap" },
+          2: { cellWidth: "auto" },
+        };
+      }
+
+      doc.autoTable({
+        head: [headerPorcentaje], // Encabezados
+        body: body, // Datos
+        startY: 20, // Posición Y donde comienza la tabla
+        theme: "grid", // Estilo de la tabla
+        styles: {
+          fontSize: 6, // Tamaño de la fuente
+          cellPadding: 1, // Espaciado interno de las celdas
         },
+        headStyles: {
+          fillColor: [71, 209, 214], // Color de fondo del encabezado
+          textColor: [255, 255, 255], // Color del texto del encabezado
+          fontStyle: "bold", // Negritas en el encabezado
+        },
+        columnStyles: columnsPorcentaje,
       });
-      
     }
-  
+
     doc.save("reporte.pdf");
   };
 
@@ -250,7 +312,11 @@ export default function PdfTablaEncuestas({ parametros, data, tipo_reporte }) {
     <button
       type="button"
       className="btn btn-outline text-danger"
-      title={ tipo_reporte=== 'completo' ? "Reporte PDF completo" : "Reporte % Asignado" }
+      title={
+        tipo_reporte === "completo"
+          ? "Reporte PDF completo"
+          : "Reporte % Asignado"
+      }
       onClick={exportToPdf}
     >
       <i className="bi bi-filetype-pdf" style={{ fontSize: "1.4rem" }}></i>
